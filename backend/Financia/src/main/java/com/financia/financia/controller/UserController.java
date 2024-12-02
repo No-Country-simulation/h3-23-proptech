767d1f245ea640e036119ed422ca36188edf7920
rudetm32 <jaruiz721818@gmail.com>
2024-12-02 10:58:42 -0600

    feat: method post and get by Id, register User

--- backend/Financia/src/main/java/com/financia/financia/controller/UserController.java
+++ backend/Financia/src/main/java/com/financia/financia/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.financia.financia.controller;
 
 
+import com.financia.financia.dto.CompleteUserDTO;
 import com.financia.financia.dto.UserAuthRegisterResponseDTO;
+import com.financia.financia.dto.UserResponseDTO;
 import com.financia.financia.dto.UserPreAuthRegisterDTO;
 import com.financia.financia.service.impl.UserServiceImpl;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -27,8 +27,25 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<UserAuthRegisterResponseDTO>  registerUser(@RequestBody @Valid UserPreAuthRegisterDTO userPreAuthRegisterDTO, UriComponentsBuilder uriComponentsBuilder){
         UserAuthRegisterResponseDTO response = userServiceImpl.initialRegister(userPreAuthRegisterDTO);
-        URI url = uriComponentsBuilder.path("/user/{id}")
+        URI url = uriComponentsBuilder.path("/users/{id}")
                 .buildAndExpand(response.id()).toUri();
         return ResponseEntity.created(url).body(response);
     }
+
+    @GetMapping("/{idUser}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable("idUser") Integer idUser) {
+        UserResponseDTO user = userServiceImpl.findUserById(idUser);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PostMapping("/{idUser}/complete-registration")
+    public ResponseEntity<?> completeRegistration(@PathVariable("idUser") Integer idUser, @RequestBody @Valid CompleteUserDTO completeUserDTO) {
+        userServiceImpl.completeRegistration(idUser, completeUserDTO);
+        return ResponseEntity.ok().build();
+    }
+
 }
