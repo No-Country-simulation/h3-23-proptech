868ce199ab59a7d55c3f258090426a86788e06ab
Leonel Amaya <omar.mejia2099@gmail.com>
2024-12-10 13:43:36 -0500

    change cors

--- backend/Financia/src/main/java/com/financia/financia/security/SecurityConfigurations.java
+++ backend/Financia/src/main/java/com/financia/financia/security/SecurityConfigurations.java
@@ -47,7 +47,7 @@ public class SecurityConfigurations {
     @Bean
     public CorsFilter corsFilter(){
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://front-hacka-indol.vercel.app/");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
